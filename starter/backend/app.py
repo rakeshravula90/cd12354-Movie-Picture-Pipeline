44295406baa369569113fa4c7a61af8da0f50380
root <rakeshravula1234@gmail.com>
2026-08-28 16:57:07 +0000

    Fix CORS and update Kubernetes deployment configuration

--- starter/backend/app.py
+++ starter/backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect
+from flask_cors import CORS
 from movies import movies_api
 
 app = Flask(__name__)
 
-app.register_blueprint(movies_api)
+CORS(app)
 
+app.register_blueprint(movies_api)
 
 @app.route("/")
 def home():
